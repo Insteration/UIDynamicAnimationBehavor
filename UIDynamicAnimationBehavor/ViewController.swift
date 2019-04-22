c492befcb88ba90c1fcb7ccdd674754e5fda39cd
Art Karma <inste@icloud.com>
2019-04-22 19:53:49 +0300

    First commit

--- UIDynamicAnimationBehavor/ViewController.swift
+++ UIDynamicAnimationBehavor/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    private var animator: UIDynamicAnimator!
+    private var snapping: UISnapBehavior! // panoramic tapped gesture
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let punGesture = UIPanGestureRecognizer(target: self, action: #selector(punAddView))
+        imageView.addGestureRecognizer(punGesture)
+        imageView.isUserInteractionEnabled = true
     }
 
+    @objc func punAddView(recognizer: UIPanGestureRecognizer) {
+        switch recognizer.state {
+        case .changed:
+            let translation = recognizer.translation(in: self.view)
+            imageView.center = CGPoint(x: imageView.center.x + translation.x, y: imageView.center.y + translation.y) // take x coordinate and take to move
+//            imageView.center = CGPoint(x: imageView.center.x + translation.x, y: imageView.center.y) // take x coordinate and take to move
+            recognizer.setTranslation(.zero, in: self.view)
+            
+
+        default:
+            ()
+        }
+    }
 
 }
 
