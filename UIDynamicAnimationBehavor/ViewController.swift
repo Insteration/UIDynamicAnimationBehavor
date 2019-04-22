4653c5ae3a205e32dfed210ddfe01bf0ecee5f9e
Art Karma <inste@icloud.com>
2019-04-22 20:11:15 +0300

    Second commit

--- UIDynamicAnimationBehavor/ViewController.swift
+++ UIDynamicAnimationBehavor/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var imageView: UIImageView!
     
     private var animator: UIDynamicAnimator!
@@ -17,25 +17,48 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        animator = UIDynamicAnimator(referenceView: self.view) // это будет происходит во вьюхе
+        snapping = UISnapBehavior(item: imageView, snapTo: self.view.center)
+        snapping.damping = 0
 
+        animator.addBehavior(snapping)
+        
+        
         let punGesture = UIPanGestureRecognizer(target: self, action: #selector(punAddView))
         imageView.addGestureRecognizer(punGesture)
         imageView.isUserInteractionEnabled = true
     }
-
+    
     @objc func punAddView(recognizer: UIPanGestureRecognizer) {
         switch recognizer.state {
+        case .began:
+            animator.removeBehavior(snapping)
         case .changed:
             let translation = recognizer.translation(in: self.view)
             imageView.center = CGPoint(x: imageView.center.x + translation.x, y: imageView.center.y + translation.y) // take x coordinate and take to move
-//            imageView.center = CGPoint(x: imageView.center.x + translation.x, y: imageView.center.y) // take x coordinate and take to move
-            recognizer.setTranslation(.zero, in: self.view)
-            
-
-        default:
-            ()
+        case .possible:
+            break
+        case .ended, .failed, .cancelled:
+            animator.addBehavior(snapping) // когда закончен жест или помломалось, то добавляется анимация
+        @unknown default:
+            fatalError()
         }
     }
-
+    
+    //    @objc func punAddView(recognizer: UIPanGestureRecognizer) {
+    //        switch recognizer.state {
+    //        case .changed:
+    //            let translation = recognizer.translation(in: self.view)
+    //            imageView.center = CGPoint(x: imageView.center.x + translation.x, y: imageView.center.y + translation.y) // take x coordinate and take to move
+    ////            imageView.center = CGPoint(x: imageView.center.x + translation.x, y: imageView.center.y) // take x coordinate and take to move
+    //            recognizer.setTranslation(.zero, in: self.view)
+    //
+    //
+    //        default:
+    //            ()
+    //        }
+    //    }
+    
 }
 
